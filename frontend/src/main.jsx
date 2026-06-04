62e1358363bab18b0dc78be8f6e88c44289ba18b
alexvera23 <alejandro.cholulao@alumno.buap.mx>
2026-06-04 09:49:30 -0600

    Instalando chart.js para las graficas en los reportes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// Importamos nuestro ThemeProvider
 import { ThemeProvider } from './context/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
