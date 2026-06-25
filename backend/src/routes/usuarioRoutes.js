2d6f9eb5dd80f1b71a991c389b60fc71affaffee
alexvera23 <alejandro.cholulao@alumno.buap.mx>
2026-06-25 10:35:47 -0600

    Blindar rutas endpoint usuarios

--- backend/src/routes/usuarioRoutes.js
+++ backend/src/routes/usuarioRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+//Importando los guardias 
+const { verificarToken } = require ('../middlewares/authMiddleware');
+const { checkRole } = require ('../middlewares/roleMiddleware');
 
 // Rutas base: /api/usuarios
-router.get('/', usuarioController.obtenerTodos);
-router.get('/:id', usuarioController.obtenerPorId);
-router.post('/', usuarioController.crearUsuario);
-router.put('/:id', usuarioController.actualizarUsuario);
-router.delete('/:id', usuarioController.eliminarUsuario);
+router.get('/', verificarToken, usuarioController.obtenerTodos);
+router.get('/:id', verificarToken, usuarioController.obtenerPorId);
+router.post('/', verificarToken, checkRole(['ADMIN']), usuarioController.crearUsuario);
+router.put('/:id', verificarToken, checkRole(['ADMIN']), usuarioController.actualizarUsuario);
+router.delete('/:id', verificarToken, checkRole(['ADMIN']), usuarioController.eliminarUsuario);
 
 module.exports = router;
