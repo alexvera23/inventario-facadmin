4796555f92531804982d2ccb6c435e79f683788c
Alex vera <alejandro.cholulao@alumno.buap.mx>
2026-06-28 19:04:02 -0600

    Proteccion en el sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -12,9 +12,9 @@ export default function Sidebar({ activeView, setActiveView, isMobileOpen, setMo
     { id: 'reportes', label: 'Reportes', icon: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z' },
   ];
 
-  //  FILTRO DE SEGURIDAD: Ocultar pestaña 'usuarios' si no es ADMIN
+  //  FILTRO DE SEGURIDAD: Ocultar pestaña 'usuarios' si no es ADMIN ESTA POR VERSE SI SI SE APLICA, EN CASO DE QUE SI PONER usuarios entre las comillas 
   const visibleNavItems = navItems.filter(item => {
-    if (item.id === 'usuarios' && user?.rol !== 'ADMIN') return false;
+    if (item.id === '' && user?.rol !== 'ADMIN') return false;
     return true;
   });
 
