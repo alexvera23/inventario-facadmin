4964390068f3e0d9a5dbf86ede185c06af8a94b7
alexvera23 <alejandro.cholulao@alumno.buap.mx>
2026-08-10 10:29:31 -0600

    Cambios de iconos en los drawer

--- frontend/src/views/Transacciones/TransaccionesDrawer.jsx
+++ frontend/src/views/Transacciones/TransaccionesDrawer.jsx
@@ -29,7 +29,12 @@ export default function TransaccionDrawer({ isOpen, onClose, transaccion, onOpen
                   {transaccion.tipo}
                 </span>
               </div>
-              <h3 className="text-lg font-heading font-bold text-text-primary mt-1">Detalle de Transacción</h3>
+              <div className="flex items-center gap-2">
+                <h3 className="text-lg font-heading font-bold text-text-primary mt-1">Detalle de Transacción</h3>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+                </svg>
+              </div>
             </div>
             
             <button 
--- frontend/src/views/Usuarios/UserDrawer.jsx
+++ frontend/src/views/Usuarios/UserDrawer.jsx
@@ -147,9 +147,14 @@ export default function UserDrawer({ isOpen, onClose, usuario, onOpenReport, onO
         
         {/* Header */}
         <div className="py-[18px] px-5 border-b border-border bg-app flex items-center justify-between">
-          <h3 className="font-heading font-extrabold text-[1rem] text-text-primary">
-            Detalle de Usuario
-          </h3>
+          <div className="flex items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+            </svg>
+            <h3 className="font-heading font-extrabold text-[1rem] text-text-primary">
+              Detalle de Usuario
+            </h3>
+          </div>
           <button onClick={onClose} className="p-1.5 rounded-lg hover:bg-inputBg text-text-secondary transition-colors">
             <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/></svg>
           </button>
